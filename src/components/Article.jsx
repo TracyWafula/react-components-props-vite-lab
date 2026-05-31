23f37e305fa96609d889aa90b53ca8131560d8de
TracyWafula <tracywafula@gmail.com>
2026-05-31 22:03:35 +0200

    Made corrections to Article component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Article({post}){
     return(
         <article>
-                <h3>{post.title}</h3>
-                <small>{post.date}</small>
-                <p>{post.preview}</p>
+            <h3>{post.title}</h3>
+            <small>{post.date}</small>
+            <p>{post.preview}</p>
         </article>
        
     )
